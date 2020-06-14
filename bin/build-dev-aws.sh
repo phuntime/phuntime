7549c09a8cd8f7f05f4c08bf0981fbaade2a6c8f
pizzaminded <pizzaminded@users.noreply.github.com>
2020-06-14 02:05:21 +0200

    building test project: improve project pipeline

--- bin/build-dev-aws.sh
+++ bin/build-dev-aws.sh
@@ -9,18 +9,29 @@ echo $CONTAINER_ID
 printf "Checking PHP version\n"
 docker exec -it $CONTAINER_ID /opt/php/bin/php -v
 
-# copy built runtime and bootstrap file to examples dir to be handled by aws-cdk
 printf "Building example runtime build dir\n"
 
-CDK_DEV_DIR=./../../resources/cdk/phuntime-build
+CDK_PROJECT_DIR=./../../resources/cdk
+CDK_RUNTIME_DIR=$CDK_PROJECT_DIR/phuntime-build
 
-mkdir -p $CDK_DEV_DIR/bin
+mkdir -p $CDK_RUNTIME_DIR/bin
 
-docker cp $CONTAINER_ID:/opt/php/bin/php $CDK_DEV_DIR/bin/php
-cp ./bootstrap $CDK_DEV_DIR/bootstrap
+docker cp $CONTAINER_ID:/opt/php/bin/php $CDK_RUNTIME_DIR/bin/php
+cp ./bootstrap $CDK_RUNTIME_DIR/bootstrap
 
-chmod +x $CDK_DEV_DIR/bootstrap
-chmod +x $CDK_DEV_DIR/bin/php
+chmod +x $CDK_RUNTIME_DIR/bootstrap
+chmod +x $CDK_RUNTIME_DIR/bin/php
 
-printf "runtime has been copied to cdk dir.\n"
-printf "run 'make deploy-aws-dev' in your repository root to build & deploy a development version to AWS.\n"
+#i havent successfully configured composer to allow install package inside itself
+printf "Copying sources to cdk project\n"
+cp ./../../composer.json $CDK_PROJECT_DIR/composer.json
+cp ./../../composer.lock $CDK_PROJECT_DIR/composer.lock
+cp -r ./../../src/Phuntime/Core $CDK_PROJECT_DIR/src/Phuntime/Core
+#let's face it , other vendors are not so heavily required here
+cp -r ./../../src/Phuntime/Aws $CDK_PROJECT_DIR/src/Phuntime/Aws
+
+cd $CDK_PROJECT_DIR && composer install
+
+
+printf "CDK project is ready to deploy.\n"
+printf "Run 'make deploy-aws-dev' in your repository root to deploy a development version to AWS.\n"
