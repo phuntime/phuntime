17decbef27314880b1f4ba0fa5978e8023408b51
pizzaminded <pizzaminded@users.noreply.github.com>
2020-07-09 23:33:43 +0200

    cleanups and gitignores

--- bin/build-dev-aws.sh
+++ bin/build-dev-aws.sh
@@ -4,21 +4,9 @@ printf "[build-dev-aws] Build START\n"
 
 
 printf "[build-dev-aws] Building runtime dir\n"
-#PHT_RUNTIME_DIR=./resources/cdk/runtime ./bin/build-aws-runtime.sh
+PHT_RUNTIME_DIR=./resources/cdk/runtime ./bin/build-aws-runtime.sh
 SOURCE_DIR=./resources/fpm-function DEST_DIR=./resources/cdk/fpm-function ./bin/build-test-function.sh
 SOURCE_DIR=./resources/function DEST_DIR=./resources/cdk/function ./bin/build-test-function.sh
-#CDK_RUNTIME_DIR=./../../resources/cdk/phuntime-build
-
-#mkdir -p $CDK_RUNTIME_DIR/bin
-
-
-
-
-#chmod +x $CDK_RUNTIME_DIR/bootstrap
-#chmod +x $CDK_RUNTIME_DIR/bin/php
-
-
-
 
 printf "Done.\n"
 printf "Functions and runtime are ready to deploy.\n"
