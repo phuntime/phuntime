4a5778610297abda6fafe4e84dbc920af3bd3c22
pizzaminded <pizzaminded@users.noreply.github.com>
2020-07-09 23:32:52 +0200

    new test function building script

--- bin/build-dev-aws.sh
+++ bin/build-dev-aws.sh
@@ -1,45 +1,25 @@
 #!/usr/bin/env bash
 
-cd ./runtime/aws || exit 1
+printf "[build-dev-aws] Build START\n"
 
-docker build -t="phuntime-lambda-build" .
-CONTAINER_ID=$(docker run -it -d phuntime-lambda-build:latest)
-echo $CONTAINER_ID
 
-printf "Checking PHP version\n"
-docker exec -it $CONTAINER_ID /opt/php/bin/php -v
-printf "Checking Swoole version\n"
-docker exec -it $CONTAINER_ID /opt/php/bin/php -i | grep swoole
-printf "Building example runtime build dir\n"
+printf "[build-dev-aws] Building runtime dir\n"
+#PHT_RUNTIME_DIR=./resources/cdk/runtime ./bin/build-aws-runtime.sh
+SOURCE_DIR=./resources/fpm-function DEST_DIR=./resources/cdk/fpm-function ./bin/build-test-function.sh
+SOURCE_DIR=./resources/function DEST_DIR=./resources/cdk/function ./bin/build-test-function.sh
+#CDK_RUNTIME_DIR=./../../resources/cdk/phuntime-build
 
-CDK_PROJECT_DIR=./../../resources/function
-CDK_RUNTIME_DIR=./../../resources/cdk/phuntime-build
+#mkdir -p $CDK_RUNTIME_DIR/bin
 
-mkdir -p $CDK_RUNTIME_DIR/bin
 
-docker cp $CONTAINER_ID:/opt/php/bin/php $CDK_RUNTIME_DIR/bin/php
-cp ./bootstrap $CDK_RUNTIME_DIR/bootstrap
 
-chmod +x $CDK_RUNTIME_DIR/bootstrap
-chmod +x $CDK_RUNTIME_DIR/bin/php
 
-#i havent successfully configured composer to allow install package inside itself
-printf "Copying sources to cdk project\n"
-cp ./../../composer.json $CDK_PROJECT_DIR/composer.json
-cp ./../../composer.lock $CDK_PROJECT_DIR/composer.lock
+#chmod +x $CDK_RUNTIME_DIR/bootstrap
+#chmod +x $CDK_RUNTIME_DIR/bin/php
 
-rm -rf $CDK_PROJECT_DIR/src/Phuntime/Core
-rm -rf $CDK_PROJECT_DIR/src/Phuntime/Aws
-rm -rf $CDK_PROJECT_DIR/src/Phuntime/Local
 
-cp -r ./../../src/Phuntime/Core $CDK_PROJECT_DIR/src/Phuntime/Core
-#let's face it , other providers are not so heavily required here
-cp -r ./../../src/Phuntime/Aws $CDK_PROJECT_DIR/src/Phuntime/Aws
-cp -r ./../../src/Phuntime/Local $CDK_PROJECT_DIR/src/Phuntime/Local
-printf "Installing fresh vendors to cdk project\n"
-cd $CDK_PROJECT_DIR && composer install
 
 
 printf "Done.\n"
-printf "CDK project is ready to deploy.\n"
+printf "Functions and runtime are ready to deploy.\n"
 printf "Run 'make deploy-aws-dev' in your repository root to deploy a development version to AWS.\n"
