28f5157249245bb1fdcedc067bebc4316f3affa3
pizzaminded <pizzaminded@users.noreply.github.com>
2020-07-11 03:32:00 +0200

    zip artifacts while building

--- bin/build-dev-aws.sh
+++ bin/build-dev-aws.sh
@@ -11,6 +11,12 @@ PHT_RUNTIME_DIR=./build/aws-runtime ./bin/build-aws-runtime.sh
 SOURCE_DIR=./resources/fpm-function DEST_DIR=./build/fpm-function ./bin/build-test-function.sh
 SOURCE_DIR=./resources/function DEST_DIR=./build/function ./bin/build-test-function.sh
 
+printf "[build-dev-aws] Zipping artifacts\n"
+(cd build/fpm-function && zip -r ../fpm-function.zip .)
+(cd build/function && zip -r ../function.zip .)
+(cd build/aws-runtime && zip -r ../aws-runtime.zip .)
+
+
 printf "Done.\n"
 printf "Functions and runtime are ready to deploy.\n"
 printf "Run 'make deploy-aws-dev' in your repository root to deploy a development version to AWS.\n"
