539a96eab6d547e283b7d3daabdf11156ace21da
pizzaminded <pizzaminded@users.noreply.github.com>
2020-07-12 00:45:34 +0200

    runtime build is now managed via makefile

--- bin/build-dev-aws.sh
+++ bin/build-dev-aws.sh
@@ -2,11 +2,10 @@
 
 printf "[build-dev-aws] Build START\n"
 
-
 printf "[build-dev-aws] Building runtime dir\n"
 rm -rf ./build/aws-runtime
 mkdir -p ./build/aws-runtime
-PHT_RUNTIME_DIR=./build/aws-runtime ./bin/build-aws-runtime.sh
+(cd ./runtime/aws &&  ARTIFACTS_DIR=$(pwd)/../../build/aws-runtime make build-runtime)
 
 SOURCE_DIR=./resources/fpm-function DEST_DIR=./build/fpm-function ./bin/build-test-function.sh
 SOURCE_DIR=./resources/function DEST_DIR=./build/function ./bin/build-test-function.sh
