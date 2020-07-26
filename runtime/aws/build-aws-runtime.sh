5c3e6c53129dbfb1c406cebc0ad5499271204026
pizzaminded <pizzaminded@users.noreply.github.com>
2020-07-26 05:47:20 +0200

    move additional fpm build steps to Makefile

--- runtime/aws/build-aws-runtime.sh
+++ runtime/aws/build-aws-runtime.sh
@@ -3,23 +3,6 @@
 # Builds AWS Lambda Compliant runtime
 #
 
-FPM_BUILD=0
-# FPM Runtime requires a little bit more work
-while getopts "f" OPTION
-do
-case $OPTION in
-f)
-  printf "[build-aws-runtime] FPM Build Requested"
-  FPM_BUILD=1
-  ;;
-*)
-  printf "[build-aws-runtime] Unknown option passed, exiting";
-  exit 1
-  ;;
-esac
-done
-
-
 printf "[build-aws-runtime] Build START\n"
 
 if [[ -z "${PHT_RUNTIME_DIR}" ]]; then
@@ -48,17 +31,6 @@ rm -rf $RUNTIME_DIR/bin/php
 mkdir -p $RUNTIME_DIR/bin/php
 docker cp $CONTAINER_ID:/opt/php $RUNTIME_DIR/bin
 cp bootstrap $RUNTIME_DIR
-
-if [ "$FPM_BUILD" -eq 1 ]; then
-   echo "[build-aws-runtime] building fpm bootstrap";
-   MODIFIED_FILES_COUNT=$(git status | grep 'modified:' | wc -l | xargs)
-   NEW_FILES_COUNT=$(git status | grep 'new file:' | wc -l | xargs)
-
-  if [ "$MODIFIED_FILES_COUNT" -ne 0 ] || [ "$NEW_FILES_COUNT" -ne 0 ]; then
-    printf "[build-aws-runtime] Build will be marked as development, as there are modified files in your project\n"
-  fi
-  cp fpm-bootstrap $RUNTIME_DIR/bootstrap
-fi
 
 chmod +x $RUNTIME_DIR/bootstrap
 chmod +x $RUNTIME_DIR/bin/php/bin/php
