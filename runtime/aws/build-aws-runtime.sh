fd815f8613c43b177883b2750e372de1a555aec5
pizzaminded <pizzaminded@users.noreply.github.com>
2020-08-04 22:10:15 +0200

    fix aws build script, add php-fpm and ini configuration to runtime dir and fix binaries paths

--- runtime/aws/build-aws-runtime.sh
+++ runtime/aws/build-aws-runtime.sh
@@ -26,13 +26,14 @@ docker exec -it $CONTAINER_ID /opt/php/bin/php -i | grep swoole
 
 # shellcheck disable=SC2059
 printf "[build-aws-runtime] Copying artifacts to ${RUNTIME_DIR}\n"
-#mkdir -p $RUNTIME_DIR/bin
-rm -rf $RUNTIME_DIR/bin/php
-mkdir -p $RUNTIME_DIR/bin/php
-mkdir -p $RUNTIME_DIR/php/lib
+mkdir -p $RUNTIME_DIR/bin
+mkdir -p $RUNTIME_DIR/php
 docker cp $CONTAINER_ID:/opt/php/ext $RUNTIME_DIR/php/ext
-docker cp $CONTAINER_ID:/opt/php/lib/php.ini $RUNTIME_DIR/php/lib/php.ini
+docker cp $CONTAINER_ID:/opt/php/bin/php $RUNTIME_DIR/bin
+docker cp $CONTAINER_ID:/opt/php/sbin/php-fpm $RUNTIME_DIR/bin/php-fpm
 cp bootstrap $RUNTIME_DIR
+cp php.ini $RUNTIME_DIR/bin
+cp php-fpm.conf $RUNTIME_DIR/php
 
 chmod +x $RUNTIME_DIR/bootstrap
 chmod +x $RUNTIME_DIR/bin/php/bin/php
