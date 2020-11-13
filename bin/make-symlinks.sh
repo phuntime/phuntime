666b71197a55451c2dbdb0ee9fcb3689eb2c250c
pizzaminded <pizzaminded@users.noreply.github.com>
2020-11-13 23:22:31 +0100

    chore: remove make-symlinks as they seem to be useless, add cleanup target

--- bin/make-symlinks.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-ln -sf $(pwd)/src/Phuntime/Core ./build/function/src/Phuntime
-ln -sf $(pwd)/src/Phuntime/Local ./build/function/src/Phuntime
-ln -sf $(pwd)/src/Phuntime/Aws ./build/function/src/Phuntime
