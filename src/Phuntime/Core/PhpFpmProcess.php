354de23aff49431277656dca1cad075c9e57228e
pizzaminded <pizzaminded@users.noreply.github.com>
2020-11-13 23:09:05 +0100

    feat: proc_open improvements
    class will now check that process returned a phrase to stdout to ensure that fpm is ready for handling requests.

--- src/Phuntime/Core/PhpFpmProcess.php
+++ src/Phuntime/Core/PhpFpmProcess.php
@@ -39,12 +39,18 @@ class PhpFpmProcess
 
     public function start()
     {
+        /**
+         * pipes used to share logs from FPM to phuntime
+         */
         $descriptors = [
             0 => ['file', 'php://stdin', 'r'],
             1 => ['file', 'php://stdout', 'w'],
             2 => ['pipe', 'w'],
         ];
 
+        /**
+         * Creates a handler FPM Process
+         */
         $this->process = proc_open(
             sprintf(
                 '%s  --nodaemonize --fpm-config /opt/php/php-fpm.conf',
@@ -53,7 +59,58 @@ class PhpFpmProcess
             $descriptors,
             $this->pipes
         );
+
+        /**
+         * Do not wait for logs from FPM, just continue if nothing passed
+         * It must be before the while loop otherwise it would be hard to determine that process is running.
+         */
         stream_set_blocking($this->pipes[2], false);
+
+        /**
+         * proc_open() only can tell if process is running, but this does not mean it can handle connections
+         * so we need to look for phrase in stdout to ensure fpm is fully loaded and ready for adventure
+         */
+        $isReady = false;
+        $readyPhraseToCatch = 'ready to handle connections';
+        while ($isReady === false) {
+            $logs = (string) $this->popFpmLogs();
+            $isReady = stripos($logs, $readyPhraseToCatch) !== false;
+        }
+        error_log('php-fpm ready');
+
+        $this->tick();
+    }
+
+
+    /**
+     * Returns output from FPM
+     * @return string|null
+     */
+    protected function popFpmLogs(): ?string
+    {
+        return stream_get_contents($this->pipes[2]);
+    }
+
+    /**
+     * checks with each tick that fpm is still working.
+     */
+    protected function checkProcessStatus():void
+    {
+        $status = proc_get_status($this->process);
+        if(!$status['running']) {
+            error_log('php-fpm stopped running for some reason');
+            //@TODO maybe throw an exception here?
+        }
+    }
+
+    public function tick(): void
+    {
+        $this->checkProcessStatus();
+        $logs = $this->popFpmLogs();
+
+        if($logs !== null) {
+            error_log($logs);
+        }
     }
 
     protected function stop()
