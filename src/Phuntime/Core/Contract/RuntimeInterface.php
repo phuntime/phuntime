f6b4d45a7433bf73ab99157acff2d89db569e391
pizzaminded <pizzaminded@users.noreply.github.com>
2021-05-07 17:56:15 +0200

    refactor: class cleanups

--- src/Phuntime/Core/Contract/RuntimeInterface.php
+++ src/Phuntime/Core/Contract/RuntimeInterface.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Phuntime\Core\Contract;
 
-use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -13,33 +12,15 @@ use Psr\Log\LoggerInterface;
 interface RuntimeInterface
 {
 
-    /**
-     * @return ContextInterface
-     */
     public function getContext(): ContextInterface;
 
-    /**
-     * Exposes a runtime-specific logger instance.
-     * @return LoggerInterface
-     */
     public function getLogger(): LoggerInterface;
 
     public function getNextEvent(): array;
 
-    /**
-     * @deprecated
-     * @param string $requestId
-     * @param ResponseInterface $response
-     * @return void
-     */
     public function respondToEvent(string $eventId, object $response): void;
 
-    /**
-     * @param \Throwable $exception
-     * @param string|null $requestId - may be null as not in any event RequestId is present
-     */
     public function handleInvocationError(\Throwable $exception, ?string $requestId = null): void;
-
 
     public function handleInitializationException(\Throwable $throwable);
 
