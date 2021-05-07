643a9d155f7cc3d27651ab8a23f3d8bf0ddb58be
pizzaminded <pizzaminded@users.noreply.github.com>
2021-05-07 21:26:49 +0200

    fix definition

--- src/Phuntime/Core/EventTransformer/SyncEventTransformerInterface.php
+++ src/Phuntime/Core/EventTransformer/SyncEventTransformerInterface.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Phuntime\Core\EventTransformer;
 
+use Phuntime\Core\Contract\EventInterface;
+
 /**
  * Used for handling Triggers and synchronous events
  * (examples: AWS Cognito Verify Auth Challenge)
@@ -11,16 +13,9 @@ namespace Phuntime\Core\EventTransformer;
 interface SyncEventTransformerInterface extends EventTransformerInterface
 {
     /**
-     * Processes input from runtime and turns them into its object-oriented version.
-     * @param array $payload
-     * @return object
-     */
-    public function payloadToEvent(array $payload): object;
-
-    /**
-     * Processes output from your function ans turns them into an array of things that would be sent back to runtime.
+     * Processes output from your function ans turns them into an object that would be sent back to runtime.
      * @param object $event
-     * @return array
+     * @return EventInterface
      */
-    public function eventToPayload(object $event): array;
+    public function transformToRuntime(object $event): EventInterface;
 }
