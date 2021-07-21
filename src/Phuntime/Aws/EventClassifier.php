195161e92f1d521363fdd4b64ef31e0cd3b9faf4
pizzaminded <pizzaminded@users.noreply.github.com>
2021-07-21 22:36:16 +0200

    feat(aws): apigw v2 event support

--- src/Phuntime/Aws/EventClassifier.php
+++ src/Phuntime/Aws/EventClassifier.php
@@ -7,7 +7,7 @@ namespace Phuntime\Aws;
 /**
  * Detects which AWS Event we have to deal with.
  * @author pizzaminded <mikolajczajkowsky@gmail.com>
- * @license
+ * @license MIT
  */
 class EventClassifier
 {
@@ -16,13 +16,30 @@ class EventClassifier
      * @param array $eventBody
      * @return bool
      */
-    public function isApiGatewayProxyEvent(array $eventBody): bool
+    public function isApiGatewayV1ProxyEvent(array $eventBody): bool
     {
         if (!isset($eventBody['httpMethod'])) {
             return false;
         }
 
         if (!array_key_exists('multiValueQueryStringParameters', $eventBody)) {
+            return false;
+        }
+
+        if(isset($eventBody['version']) && $eventBody['version'] !== '1.0') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isApiGatewayV2ProxyEvent(array $eventBody): bool
+    {
+        if(isset($eventBody['version']) && $eventBody['version'] !== '2.0') {
+            return false;
+        }
+
+        if(!isset($eventBody['requestContext'])) {
             return false;
         }
 
