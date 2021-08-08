b48a6c78365bd5fde275c46af4bf2f5530ae70e3
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-08 16:31:31 +0200

    feat(aws-rie): append request ID to event

--- resources/aws-rie/index.php
+++ resources/aws-rie/index.php
@@ -7,14 +7,22 @@ if(!extension_loaded('swoole')) {
 
 $http = new swoole_http_server('127.0.0.1', 9001);
 $http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
+    $requestId = md5(uniqid('', true));
     $method = $request->server['request_method'];
     $pathInfo = $request->server['path_info'];
 
-    error_log(sprintf('[debug] request: %s %s', $method, $pathInfo));
+    error_log(sprintf('[info] request: %s %s', $method, $pathInfo));
 
     if($method === 'GET' && $pathInfo === '/2018-06-01/runtime/invocation/next') {
+        error_log('[info] AWS RIE matched');
+        error_log(sprintf('[debug] request id: %s', $requestId));
+
+        $requestJson = json_decode(file_get_contents(__DIR__.'/../../tests/fixtures/aws-apigateway-v1-event-1.json'), true);
+        $requestJson['requestContext']['requestId'] = $requestId;
+
+
         $response->header("Content-Type", "application/json");
-        $response->end(file_get_contents(__DIR__.'/../../tests/fixtures/aws-apigateway-v1-event-1.json'));
+        $response->end(json_encode($requestJson));
     }
 
 });
