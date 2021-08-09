d8084e9e3edfb5a4660a6bd27644ccb3ed527960
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-09 21:00:49 +0200

    feat: add setters

--- src/Phuntime/Aws/Type/ApiGatewayProxyResult.php
+++ src/Phuntime/Aws/Type/ApiGatewayProxyResult.php
@@ -29,4 +29,46 @@ class ApiGatewayProxyResult
             'isBase64Encoded' => $this->base64Encoded
         ];
     }
+
+    /**
+     * @param int $statusCode
+     */
+    public function setStatusCode(int $statusCode): void
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders(array $headers): void
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @param array $multiValueHeaders
+     */
+    public function setMultiValueHeaders(array $multiValueHeaders): void
+    {
+        $this->multiValueHeaders = $multiValueHeaders;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @param bool $base64Encoded
+     */
+    public function setBase64Encoded(bool $base64Encoded): void
+    {
+        $this->base64Encoded = $base64Encoded;
+    }
+
+
 }
