ccc3b498a744b5dfb982284ddc9303747978fc11
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-20 22:37:25 +0200

    refactor: stricter json processing

--- tests/Phuntime/UnitTestHelper.php
+++ tests/Phuntime/UnitTestHelper.php
@@ -3,15 +3,20 @@ declare(strict_types=1);
 
 namespace Phuntime;
 
+use function json_decode;
 
 class UnitTestHelper
 {
+    /**
+     * @throws \JsonException
+     */
     public static function getJsonFixture(string $fixtureName): array
     {
         return json_decode(
-            file_get_contents(
-                __DIR__ . '/../fixtures/' . $fixtureName . '.json'),
-            true
+            file_get_contents(__DIR__ . '/../fixtures/' . $fixtureName . '.json'),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
         );
     }
 }
