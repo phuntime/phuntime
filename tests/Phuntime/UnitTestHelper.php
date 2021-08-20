af512e82a4752b7a5c6ad0761c8d4fa975ae6807
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-20 22:40:42 +0200

    refactor: better fixtures processing

--- tests/Phuntime/UnitTestHelper.php
+++ tests/Phuntime/UnitTestHelper.php
@@ -12,8 +12,14 @@ class UnitTestHelper
      */
     public static function getJsonFixture(string $fixtureName): array
     {
+        $path = __DIR__ . '/../fixtures/' . $fixtureName . '.json';
+
+        if(!file_exists($path)) {
+            throw new \LogicException(sprintf('json fixture "%s" not found.', $fixtureName));
+        }
+
         return json_decode(
-            file_get_contents(__DIR__ . '/../fixtures/' . $fixtureName . '.json'),
+            file_get_contents($path),
             true,
             512,
             JSON_THROW_ON_ERROR
