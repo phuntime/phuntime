5b8b7cd5400b8a2c6e9fa81e3fd1a5b6a8e0bdf4
pizzaminded <pizzaminded@users.noreply.github.com>
2021-09-14 00:24:41 +0200

    feat: passing headers to PSR request

--- src/Phuntime/Bridge/Aws/ApiGatewayPsrBridge.php
+++ src/Phuntime/Bridge/Aws/ApiGatewayPsrBridge.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Phuntime\Bridge\Aws;
 
 
+use Nyholm\Psr7\Stream;
 use Phuntime\Aws\Type\ApiGatewayProxyEvent;
 use Phuntime\Aws\Type\ApiGatewayProxyResult;
 use Phuntime\Aws\Type\ApiGatewayV2ProxyEvent;
@@ -20,7 +21,8 @@ class ApiGatewayPsrBridge
     public function __construct(
         private ServerRequestFactoryInterface $requestFactory
     )
-    {}
+    {
+    }
 
     /**
      * @TODO: add tests for v2 event
@@ -31,19 +33,31 @@ class ApiGatewayPsrBridge
     {
         $qs = [];
 
-        if($event instanceof ApiGatewayProxyEvent) {
+        if ($event instanceof ApiGatewayProxyEvent) {
             $qs = $event->getMultiValueQueryStringParameters();
         }
 
-        if($event instanceof ApiGatewayV2ProxyEvent) {
+        if ($event instanceof ApiGatewayV2ProxyEvent) {
             $qs = $event->getQueryStringParameters();
         }
 
-        return
-            $this->requestFactory->createServerRequest(
+
+        $request = $this
+            ->requestFactory
+            ->createServerRequest(
                 $event->getHttpMethod(),
                 $event->buildUrl()
-            )->withQueryParams($qs);
+            )
+            ->withQueryParams($qs)
+            ->withBody(
+                Stream::create((string)$event->getBody())
+            );
+
+        foreach ($event->getHeaders() as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
     }
 
     public function psr7ResponseToApiGw(ResponseInterface $response, int $apiGwVersion): ApiGatewayProxyResult
@@ -52,11 +66,11 @@ class ApiGatewayPsrBridge
         $result->setBody((string)$response->getBody());
         $result->setStatusCode($response->getStatusCode());
         $result->setBase64Encoded(false);
-        if($apiGwVersion === 1) {
+        if ($apiGwVersion === 1) {
             $result->setMultiValueHeaders($response->getHeaders());
         }
 
-        if($apiGwVersion === 2) {
+        if ($apiGwVersion === 2) {
             $headers = [];
             foreach ($response->getHeaders() as $headerName => $headerValue) {
                 $headers[$headerName] = reset($headerValue);
@@ -64,7 +78,6 @@ class ApiGatewayPsrBridge
 
             $result->setHeaders($headers);
         }
-        error_log(json_encode($response->getHeaders()));
 
         return $result;
     }
